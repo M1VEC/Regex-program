79c015730059b0f49909653c0af04c428160cd7c
M1VEC <pbilton85@gmail.com>
2020-09-15 11:10:22 +0100

    Created my own regex checker and tests

---
+++ src/com/pbilton/regex/Regex_Checker.java
@@ -0,0 +1,84 @@
+package com.pbilton.regex;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Regex_Checker {
+    private static String regex;
+    private static String string;
+    private static Pattern pattern;
+    private static Matcher matcher;
+    private static boolean validRegex;
+    private static boolean doneMatching;
+    private static boolean matchFound;
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void Regex_Checker(){
+        System.out.println("***Regex Checker***");
+        do{
+            Regex_Input();
+            doneMatching = false;
+            matchFound = false;
+            while(!doneMatching) {
+                string_Input();
+                if (matchFound == true)
+                    System.out.println("Match Found");
+                else
+                    System.out.println("No match found");
+            }
+        } while (Continue());
+    }
+
+    //Overload method to pass a predetermined string and pattern
+    public static boolean Regex_Checker(String string, String regex) {
+        validateInput(regex);
+        return matcher(string);
+    }
+
+    private static void Regex_Input(){
+        do{
+            System.out.println("Enter regex expression: ");
+            regex = scanner.nextLine();
+            validRegex = validateInput(regex);
+        } while (!validRegex);
+    }
+
+    private static boolean validateInput(String input){
+        try {
+            pattern = Pattern.compile(input);
+            return true;
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    private static void string_Input(){
+        System.out.println("Enter string: ");
+        string = scanner.nextLine();
+        if (string.length() == 0)
+            doneMatching = true;    //if string is blank, ends current Regex checker
+        else
+            matcher(string);
+    }
+
+    private static boolean matcher(String input){
+        matcher = pattern.matcher(input);
+        if (matcher.matches()){
+            return matchFound = true;
+        }
+        else {
+            return matchFound = false;
+        }
+    }
+
+    private static boolean Continue(){
+        System.out.println("New expression: Y or N");
+        String reply = scanner.nextLine();
+        if (reply.equalsIgnoreCase("Y"))
+            return true;
+        return false;
+    }
+}
--- src/com/pbilton/test/com/pbilton/regex/testRegex.java
+++ src/com/pbilton/test/com/pbilton/regex/testRegex.java
@@ -1,9 +1,15 @@
 package com.pbilton.regex;
 import org.junit.Test;
 
+import static com.pbilton.regex.Regex_Checker.Regex_Checker;
+import static org.junit.Assert.assertEquals;
+
 public class testRegex {
 
     @Test
     public void testRegex(){
+        assertEquals(true, Regex_Checker("23:45", "\\d\\d:\\d\\d"));
+        assertEquals(false, Regex_Checker("23:45", "\\d:\\d\\d"));
+
     }
 }
